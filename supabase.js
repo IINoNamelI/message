8d89fd15f2b7f92775a80f83fccf26e20f018295
IINoNamelI <skorpion2021.ru@gmail.com>
2026-07-23 05:02:44 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,4 @@
-// Было:
-// import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
-// Стало:
-import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 
 const SUPABASE_URL = 'https://ekcwrmfsgcgbusqmadji.supabase.co'
