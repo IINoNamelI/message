afff8bb0501c8c7e9e29d53c95a4a9f409a5480a
IINoNamelI <skorpion2021.ru@gmail.com>
2026-07-23 05:00:35 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,8 @@
-import { createClient } from 'https://cdn.jsdelivr.com/npm/@supabase/supabase-js/+esm'
+// Было:
+// import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+// Стало:
+import { createClient } from 'https://unpkg.com/@supabase/supabase-js@2'
+
 
 const SUPABASE_URL = 'https://ekcwrmfsgcgbusqmadji.supabase.co'
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVrY3dybWZzZ2NnYnVzcW1hZGppIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ3MzUxOTMsImV4cCI6MjEwMDMxMTE5M30.YbqLJtVPTnULpRZxe8nGgI6QZnT7qKMY0Whr84BaUFU'
